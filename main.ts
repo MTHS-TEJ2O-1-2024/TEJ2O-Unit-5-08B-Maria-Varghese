8080dcec2ff4ce5a418e0fcbe994557305718952
Maria-Varghese <maria.varghese@mths.ca>
2024-12-04 11:13:09 -0500

    Program using stepper motors and sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,37 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria Varghese
+ * Created on: Sep 2024
+ * This program uses stepper motor
 */
 
-basic.showString('Hello, World!')
+//variable
+let distanceToObject: number = 0
+
+//calculating distanceToObject and moving the stepper motor  
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    basic.showNumber(distanceToObject)
+    basic.showString(' cm')
+    basic.showIcon(IconNames.Happy)
+
+    if (distanceToObject > 10) {
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(50, 48)
+
+    } else {
+        basic.showIcon(IconNames.No)
+        robotbit.StpCarMove(10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        robotbit.StpCarMove(50, 48)
+    }
+})
+
+//cleanup
+basic.showIcon(IconNames.Happy)
